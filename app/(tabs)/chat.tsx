18834b4a007a9e4ff929ac617b1d196681e4613a
dlcks0601 <dlcks0601@gmail.com>
2025-04-30 11:06:24 +0900

    [Feat] 탭 화면 요소 추가

---
+++ app/(tabs)/chat.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export default function ChatScreen() {
+  return (
+    <View className='flex-1 bg-[#121212]'>
+      <View className='flex-1 items-center justify-center px-4'>
+        <Text className='text-white'>Chat</Text>
+      </View>
+    </View>
+  );
+}
---
+++ app/(tabs)/playlist.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export default function PlaylistScreen() {
+  return (
+    <View className='flex-1 bg-[#121212]'>
+      <View className='flex-1 items-center justify-center px-4'>
+        <Text className='text-white'>Playlist</Text>
+      </View>
+    </View>
+  );
+}
