f9f80f0d13571714583a19b8e896a0e94de71970
dlcks0601 <dlcks0601@gmail.com>
2025-04-30 11:00:25 +0900

    [Refactor] 루트 레이아웃 리팩토링 및 쿼리

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,21 @@
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import { useCallback } from 'react';
-import Header from './components/Header';
+import { useCallback, useEffect } from 'react';
+import authStore from '@/store/authStore';
 import '../global.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 SplashScreen.preventAutoHideAsync();
 
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
+  const router = useRouter();
+  const { jwt } = authStore();
+
   const [fontsLoaded] = useFonts({
     Pretendard: require('../assets/fonts/Pretendard-Regular.otf'),
     'Pretendard-Thin': require('../assets/fonts/Pretendard-Thin.otf'),
@@ -29,23 +35,35 @@ export default function RootLayout() {
     }
   }, [fontsLoaded]);
 
+  useEffect(() => {
+    if (!fontsLoaded) return;
+
+    if (jwt.accessToken) {
+      router.replace('/(tabs)');
+    } else {
+      router.replace('/(auth)/login');
+    }
+  }, [jwt.accessToken, fontsLoaded, router]);
+
   if (!fontsLoaded) {
     return null;
   }
 
   return (
-    <SafeAreaView
-      edges={['top', 'bottom']}
-      className='flex-1 bg-black'
-      onLayout={onLayoutRootView}
-    >
-      <StatusBar style='light' />
-      <Header title='PLIFY' />
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      />
-    </SafeAreaView>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaView
+        edges={['top', 'bottom']}
+        className='flex-1 bg-[#121212]'
+        onLayout={onLayoutRootView}
+      >
+        <StatusBar style='light' />
+
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        />
+      </SafeAreaView>
+    </QueryClientProvider>
   );
 }
