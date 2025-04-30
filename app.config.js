25c53b3cc300164d533598ff9698bd334a672d3f
dlcks0601 <dlcks0601@gmail.com>
2025-04-30 10:56:46 +0900

    [Add] 의존성 설치 및 설정

---
+++ app.config.js
@@ -0,0 +1,61 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: 'plify-app',
+    slug: 'plify-app',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'plify',
+    userInterfaceStyle: 'automatic',
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/images/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+    },
+    web: {
+      bundler: 'metro',
+      output: 'static',
+      favicon: './assets/images/favicon.png',
+    },
+    plugins: [
+      'expo-router',
+      [
+        'expo-splash-screen',
+        {
+          image: './assets/images/splash-icon.png',
+          imageWidth: 200,
+          resizeMode: 'contain',
+          backgroundColor: '#ffffff',
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    fonts: {
+      Pretendard: './assets/fonts/Pretendard-Regular.otf',
+      'Pretendard-Thin': './assets/fonts/Pretendard-Thin.otf',
+      'Pretendard-ExtraLight': './assets/fonts/Pretendard-ExtraLight.otf',
+      'Pretendard-Light': './assets/fonts/Pretendard-Light.otf',
+      'Pretendard-Medium': './assets/fonts/Pretendard-Medium.otf',
+      'Pretendard-SemiBold': './assets/fonts/Pretendard-SemiBold.otf',
+      'Pretendard-Bold': './assets/fonts/Pretendard-Bold.otf',
+      'Pretendard-ExtraBold': './assets/fonts/Pretendard-ExtraBold.otf',
+      'Pretendard-Black': './assets/fonts/Pretendard-Black.otf',
+      SpaceMono: './assets/fonts/SpaceMono-Regular.ttf',
+    },
+    extra: {
+      API_URL: process.env.API_URL,
+      SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+      SPOTIFY_REDIRECT_URI: process.env.SPOTIFY_REDIRECT_URI,
+      SPOTIFY_SCOPES: process.env.SPOTIFY_SCOPES,
+    },
+  },
+};
