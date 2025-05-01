e4ecf36137f36db4b11d5ad028eb77281eeb9683
dlcks0601 <dlcks0601@gmail.com>
2025-05-01 21:40:23 +0900

    [Feat] 키보드 추가

--- app/components/playlist/CommentsTab.tsx
+++ app/components/playlist/CommentsTab.tsx
@@ -1,4 +1,11 @@
-import { View, ScrollView } from 'react-native';
+import {
+  View,
+  Keyboard,
+  Platform,
+  TouchableOpacity,
+  Text,
+  Button,
+} from 'react-native';
 import {
   useComments,
   useLikeComment,
@@ -6,7 +13,7 @@ import {
   useAddComment,
 } from '@/hooks/playlist.query';
 import { useLocalSearchParams } from 'expo-router';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import * as Haptics from 'expo-haptics';
 import useAuthStore from '@/store/authStore';
 import CommentItem from './CommentItem';
@@ -21,6 +28,23 @@ export default function CommentsTab() {
   const addComment = useAddComment();
   const [selectedComment, setSelectedComment] = useState<number | null>(null);
   const { userInfo } = useAuthStore();
+  const [isKeyboardVisible, setKeyboardVisible] = useState(false);
+
+  useEffect(() => {
+    const keyboardWillShow = Keyboard.addListener(
+      Platform.OS === 'ios' ? 'keyboardWillShow' : 'keyboardDidShow',
+      () => setKeyboardVisible(true)
+    );
+    const keyboardWillHide = Keyboard.addListener(
+      Platform.OS === 'ios' ? 'keyboardWillHide' : 'keyboardDidHide',
+      () => setKeyboardVisible(false)
+    );
+
+    return () => {
+      keyboardWillShow.remove();
+      keyboardWillHide.remove();
+    };
+  }, []);
 
   const handleLongPress = async (commentId: number) => {
     await Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
@@ -59,7 +83,8 @@ export default function CommentsTab() {
 
   return (
     <View className='flex-1 bg-[#121212]'>
-      <ScrollView className='flex-1 p-4'>
+      <View className='flex-1 items-center'></View>
+      <View className='flex-1 p-4'>
         {comments?.comment.map((comment) => (
           <CommentItem
             key={comment.commentId}
@@ -68,9 +93,7 @@ export default function CommentsTab() {
             onLike={() => handleLike(comment.commentId.toString())}
           />
         ))}
-      </ScrollView>
-
-      <CommentInput onSubmit={handleAddComment} />
+      </View>
 
       <ActionModal
         visible={selectedComment !== null}
