9fcbaaa1b9ca98f548664d20d4c1f99fd95b88c2
dlcks0601 <dlcks0601@gmail.com>
2025-05-08 04:21:30 +0900

    [Refactor] 로그인 화면 UI 변경: 로고 및 텍스트 수정, 버튼 패딩 추가

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -12,30 +12,10 @@ export default function LoginScreen() {
       <View className='flex-1 items-center justify-between py-8'>
         <View className='flex-1 items-center justify-center gap-6'>
           <View className='items-center gap-4'>
-            <Logo width={160} height={40} />
-            <Text className='text-md text-gray-400'>
-              PLIFY는 Spotify를 사용하는 사람들을 위한 공간이에요.
-            </Text>
-          </View>
-          <View className='flew-row items-start gap-4 px-6'>
-            <View className='px-[20px] py-[7px] rounded-3xl bg-green-200 '>
-              <Text className='text-md font-normal text-green-800'>
-                가장 많이 들은 곡을 알고싶어요.
-              </Text>
-            </View>
-            <View className='px-[20px] py-[7px] rounded-3xl bg-yellow-200 '>
-              <Text className='text-md font-normal text-yellow-800'>
-                가장 많이 들은 가수를 알고싶어요.
-              </Text>
-            </View>
-            <View className='px-[20px] py-[7px] rounded-3xl bg-blue-200 '>
-              <Text className='text-md font-normal text-blue-800'>
-                플레이리스트를 공유하고 싶어요.
-              </Text>
-            </View>
+            <Text className='text-7xl font-logo text-white'>PLIO</Text>
           </View>
         </View>
-        <View className='w-full'>
+        <View className='w-full px-4 py-4'>
           <SpotifyLoginButton onPress={handleSpotifyLogin} />
         </View>
       </View>
