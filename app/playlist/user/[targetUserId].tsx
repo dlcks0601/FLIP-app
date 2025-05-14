234fd90d08513edddbda0d1c13ca51eaecb9dded
leechan <dlcks0601@gmail.com>
2025-05-14 23:55:46 +0900

    [Refactor] 사용자 플레이리스트 화면 수정: anotherUser 데이터 구조로 변경하여 사용자 이름 및 플레이리스트 표시 방식 개선

--- app/playlist/user/[targetUserId].tsx
+++ app/playlist/user/[targetUserId].tsx
@@ -7,7 +7,7 @@ import { useAnotherUserPageQuery } from '@/hooks/mypage.query';
 export default function UserPlaylistsScreen() {
   const { type, targetUserId } = useLocalSearchParams();
   const router = useRouter();
-  const { data: playlists } = useAnotherUserPageQuery(targetUserId as string);
+  const { data: anotherUser } = useAnotherUserPageQuery(targetUserId as string);
   targetUserId as string;
 
   return (
@@ -19,15 +19,15 @@ export default function UserPlaylistsScreen() {
         <View className='flex-1'>
           <Text className='text-white text-xl font-bold text-center'>
             {type === 'my'
-              ? `${targetUserId} 플레이리스트`
-              : `${targetUserId} 좋아요한 플레이리스트`}
+              ? `${anotherUser?.user.name} 플레이리스트`
+              : `${anotherUser?.user.name} 좋아요한 플레이리스트`}
           </Text>
         </View>
         <View className='w-10' />
       </View>
 
       <ScrollView>
-        {playlists?.playlistData?.map((playlist) => (
+        {anotherUser?.playlistData?.map((playlist) => (
           <MyPlaylistItem key={playlist.postId} playlist={playlist} />
         ))}
       </ScrollView>
